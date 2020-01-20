651d4dfe60d707c041dd6e397ea04a87df3b3e2d
GiorgioMariani <giorgiomariani94@gmail.com>
2020-01-20 16:52:19 +0100

    update on the code of adversarial perturbations generator

--- src/adversarial/adversarial_generator.py
+++ src/adversarial/adversarial_generator.py
@@ -1,79 +1,167 @@
+import networkx as nx
 import numpy as np
 import tqdm
 import torch
 import torch.nn.functional as func
 
-from  mesh.laplacian import laplacebeltrami_FEM
+import utils
 
-def find_perturbation(
-    x:torch.Tensor,
-    edge_index:torch.IntTensor,
-    faces:torch.IntTensor,
-    target:int,
-    classifier:torch.nn.Module,
-    max_c=100,
-    bsearch_iterations=20,
-    optim_iterations=500):
+class CarliniAdversarialGenerator(object):
+  def __init__(self,
+      pos:torch.Tensor,
+      edges:torch.LongTensor,
+      faces:torch.LongTensor,
+      target:int,
+      classifier:torch.nn.Module,
+      eigs_num:int=150,
+      smoothness_coeff:float=1,
+      adversarial_coeff:float=1):
+    super().__init__()
+    utils.check_data(pos, edges, faces)
+    float_type = pos.dtype
+
+    self.pos = pos
+    self.faces = faces
+    self.edges = edges
+    self.target = torch.tensor([target], device=pos.device, dtype=torch.long)
+    self.classifier = classifier
+
+    #constants
+    self.vertex_count = pos.shape[0]
+    self.edge_count = edges.shape[0]
+    self.face_count = faces.shape[0]
+
+    # compute useful data
+    self.L = tgeo.utils.get_laplacian(edges.t(), normalization="rw")
+    self.stiff, self.area = mesh.laplacian.LB_v2(self.pos, self.faces)
     
-    vertex_count = x.shape[0]
-    r = torch.normal(0, std=0.001, size=[vertex_count, 3], requires_grad=True, device=x.device)
+    # compute spectral information
+    ri,ci = stiff[0]
+    S = scipy.sparse.csr_matrix( (sv, (ri,ci)), shape=(n,n))
+    ri,ci = area[0]
+    A = scipy.sparse.csr_matrix( (av, (ri,ci)), shape=(n,n))
+    e, phi = slinalg.eigsh(S, M=A, k=eigs_num, sigma=-1e-6)
+    self.eigvals = torch.tensor(e, dtype=float_type)
+    self.eigvecs = torch.tensor(phi, dtype=float_type)
 
-    range_min, range_max = 0, max_c
-    c_optimal,r_optimal = None, None
-    for i in range(bsearch_iterations):
-        midvalue = (range_min+range_max)/2
-        print("binary search step: "+str(i+1))
-        print("c value: "+str(midvalue))
-        
-        r_optim, adv_loss, dist_loss = _optimize_perturbation(
-            x=x,f=faces,
-            r=r, t=target,
-            C=classifier,
-            iter_num=optim_iterations, 
-            c=midvalue)
-        print("adversarial loss: "+str(adv_loss))
-        print("Distorsion loss: "+str(dist_loss))
+    # other info
+    self._zero = torch.zeros([1], device=pos.device, dtype=pos.dtype)
+    self._smooth_L1_criterion = torch.nn.SmoothL1Loss(reduction='mean')
 
-        if adv_loss <= 0:
-            range_max = midvalue
-            c_optimal = midvalue
-            r_optimal = r_optim
-        else:
-            range_min = midvalue
-    return c_optimal, r_optimal
+    self._r = torch.zeros([self.vertex_count], device=pos.device, dtype=pos.dtype, requires_grad=True)
+    self._loss_values = []
+    self._iteration = 0
+    
+    def total_loss(self, Z):
+      adversarial_loss = self.adversarial_loss()
+      laplace_beltrami_loss = self.LB_loss()
+      least_meshes_loss = self.LSM_loss()
+      loss = adversarial_loss + laplace_beltrami_loss + least_meshes_loss
+      self._loss_values.append(
+        {"adversarial":adversarial_loss.item(), 
+        "laplace-beltrami":laplace_beltrami_loss.item(),
+        "least-square-meshes":least_meshes_loss.item()})
+      return loss
 
-def _optimize_perturbation(
-    x:torch.Tensor,
-    f:torch.Tensor,
-    r:torch.Tensor,
-    t:int, 
-    C:torch.nn.Module,
-    iter_num=1000,
-    c=1.0):
+  def perturbed_input(self):
+    return self.pos + self._r
+  
+  def LSM_loss(self, r:torch.Tensor):
+    n = self.vertex_count
+    tmp = tsparse.spmm(*self.L, n, n, self._r) #Least square Meshes problem 
+    return (tmp**2).sum()
 
-    r = r.clone().detach().requires_grad_(True)
-    optimizer = torch.optim.Adam([r], lr=1e-3, weight_decay=5e-4)
+  def LB_loss(self, r:torch.Tensor):
+    n = self.vertex_count
+    stiff_r, area_r = mesh.laplacian.LB_v2(self.perturbed_input(), self.faces)
+    ai, av = self.area
+    ai_r, av_r = area_r
+    _,L = tsparse.spspmm(ai, torch.reciprocal(av), *self.stiff, n, n, n)
+    _,L_r = tsparse.spspmm(ai_r, torch.reciprocal(av_r), *stiff_r, n, n, n)
+    loss = self._smooth_L1_criterion(L, L_r)
+    return loss
+
+  def adversarial_loss(self) -> torch.Tensor:
+    self.classifier(self.perturbed_input())
+    values, index = torch.sort(Z, dim=0)
+    argmax = index[-1] if index[-1] != self.target else index[-2] # max{Z(i): i != target}
+    Ztarget, Zmax = Z[self.target], Z[argmax]
+    return torch.max(Zmax - Ztarget, self.zero)
+
+  def generate(self, iter_num:int=1000, lr=8e-6) -> torch.Tensor:
+    self._r = torch.zeros([self.vertex_count], 
+      device=self.pos.device,
+      dtype=self.pos.dtype,
+      requires_grad=True)
+    self._iteration = 0
+    self._loss_values = []
+
 
     # compute gradient w.r.t. the perturbation
-    C.eval()
+    optimizer = torch.optim.Adam([self._r], lr=lr)
+    self.classifier.eval()
     for i in range(iter_num):
         optimizer.zero_grad()
-        Z = C(x+r)
-        #loss = c*_adversarial_loss(Z, t, 0) + _vector_field_norm(r)
-        loss = c*_adversarial_loss(Z, t) + _area_difference(x, f, r)
+        loss = self.total_loss()
         loss.backward()
         optimizer.step()
-    Z=C(x+r)
-    return r, _adversarial_loss(Z, t).item(), _area_difference(x, f, r).item()
+    
+    # print the losses for the last iteration
+    for key,value in self._loss_values[-1]:
+      print(key+": "+str(value))
+    return r.clone().detach(), self.adversarial_loss().clone().detach()
 
-def _adversarial_loss(Z:torch.Tensor, target:int, k=0):
-    values, index = torch.sort(Z, dim=0)
-    argmax = index[-1] if index[-1] != target else index[-2]
-    Zt = Z[target]
-    Zmax = Z[argmax]
-    return torch.max(Zmax - Zt, torch.tensor([-k], device=Z.device, dtype=Z.dtype))
+def estimate_perturbation(
+  pos:torch.Tensor,
+  edges:torch.LongTensor,
+  faces:torch.LongTensor,
+  target:int,
+  classifier:torch.nn.Module,
+  search_iterations=20,
+  minimization_iterations=1000,
+  starting_c:float=1,
+  smoothness_coeff:float=1,
+  eigs_num:int=150):
 
-# disturbance metrics --------------------------------------------------
-def _vector_field_norm(r:torch.Tensor):
-    norm = r.norm(dim=1)
-    return norm.sum()
+  range_min, range_max = 0, starting_c
+  c_optimal = None 
+  r_optimal = None
+  increasing = True # flag used to detected whether it is the first phase or the second phase 
+
+  for i in range(bsearch_iterations):
+    midvalue = (range_min+range_max)/2
+    c = range_max if increasing else midvalue
+
+    print("\nbinary search step: "+str(i+1))
+    print("range: [{},{}]\nc value: {}".format(range_min, range_max, c))
+    print("iterations per step: {}".format(optim_iterations))
+    print("phase: "+ ("incrementing" if increasing else "search"))
+
+    adv_generator = CarliniAdversarialGenerator(
+      pos=pos,
+      edges=edges,
+      faces=faces,
+      target=target,
+      classifier=classifier,
+      smoothness_coeff=smoothness_coeff,
+      adversarial_coeff=c)
+    r, adversarial_loss = adv_generator.generate(iter_num=optim_iterations)
+
+    # update best estimation
+    if adversarial_loss <= 0:
+      c_optimal, r_optimal = c, r
+
+    # update loop variables
+    if increasing and adversarial_loss > 0:
+      range_min = range_max
+      range_max = range_max*2
+    elif increasing and adversarial_loss <= 0:
+      increasing = False
+    else:
+      range_max = range_max if adversarial_loss > 0 else midvalue
+      range_min = midvalue  if adversarial_loss > 0 else range_min
+
+  # if unable to find a good c,r pair, return the best found solution
+  if r_optimal is None:
+    c_optimal, r_optimal = c, r
+  return c_optimal, r_optimal
---
+++ src/utils.py
@@ -0,0 +1,38 @@
+
+def check_data(pos:torch.Tensor, edges:torch.Tensor, faces:torch.Tensor, float_type:type=torch.double):
+    # check input consistency 
+    if len(pos.shape)!= 2 or pos.shape[1] != 3 or pos.dtype != float_type:
+      raise ValueError("The vertices matrix must have shape [n,3] and type float!")
+    if len(edges.shape) != 2 or edges.shape[1] != 2 or edges.dtype != torch.long:
+      raise ValueError("The edge index matrix must have shape [m,2] and type long!")
+    if len(faces.shape) != 2 or faces.shape[1] != 3 or faces.dtype != torch.long:
+      raise ValueError("The edge index matrix must have shape [#faces,3] and type long!")
+
+
+def get_spirals(
+  pos:torch.Tensor, 
+  edges:torch.LongTensor,
+  neighbors_num:int=256):
+
+  if len(pos.shape)!= 2 or pos.shape[1] != 3 or pos.dtype != torch.float:
+      raise ValueError("The vertices matrix must have shape [n,3] and type float!")
+  if len(edges.shape) != 2 or edges.shape[1] != 2 or edges.dtype != torch.long:
+      raise ValueError("The edge index matrix must have shape [m,2] and type long!")
+
+  n = pos.shape[0]
+  m = edges.shape[0]
+  k = neighbors_num
+
+  graph = nx.Graph()
+  graph.add_nodes_from(range(n))
+  graph.add_edges_from(edges.numpy())
+
+  N = torch.Tensor([n,k])
+  for node_index in range(n):
+    spiral = nx.single_source_shortest_path_length(G, node, cutoff=K)
+    N[node_index, :] = spiral
+  return N
+
+
+ 
+
