59a7f7835a56b5f95bb8d5876a2ee8af919a8028
giorgio <giorgiomariani94@gmail.com>
2020-02-06 14:25:50 +0100

    simple indentation fix

--- src/train.py
+++ src/train.py
@@ -80,19 +80,19 @@ def evaluate(
             if translate: mesh.transforms.transform_translation_(x)
             if rotate: mesh.transforms.transform_rotation_(x)
 
-      for i in tqdm.trange(len(eval_data)):
-          x = evaldata_pos[i]
-          y = evaldata_gtruth[i]
+        for i in tqdm.trange(len(eval_data)):
+            x = evaldata_pos[i]
+            y = evaldata_gtruth[i]
 
-          out:torch.Tensor = classifier(x)
-          if confusion is None:
-              num_classes = out.shape[-1]
-              confusion = torch.zeros([num_classes, num_classes])
-          
-          _, prediction = out.max(dim=-1)
-          target = int(y)
-          confusion[target, prediction] +=1
-          
-          correct = torch.diag(confusion).sum()
-          accuracy = correct/confusion.sum()
+            out:torch.Tensor = classifier(x)
+            if confusion is None:
+                num_classes = out.shape[-1]
+                confusion = torch.zeros([num_classes, num_classes])
+            
+            _, prediction = out.max(dim=-1)
+            target = int(y)
+            confusion[target, prediction] +=1
+            
+            correct = torch.diag(confusion).sum()
+            accuracy = correct/confusion.sum()
     return accuracy, incorrect_classes
